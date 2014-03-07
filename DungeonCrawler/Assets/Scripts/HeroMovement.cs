014d452a13bae435201a8ba628157155b0baa661
Lystlund <raslystlund@gmail.com>
2014-03-07 10:39:16 +0100

    Unity Placeholders added and HeroMovement added
    A placeholder for the hero have been added into Unity, and a script have
    been made to give the hero simple movement (up, down, left right)

---
+++ DungeonCrawler/Assets/Scripts/HeroMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Velocity vectors to give movement to the hero
+		if (Input.GetKey(KeyCode.UpArrow)){
+			rigidbody.velocity = new Vector3(0,1,0);
+		}
+		else if (Input.GetKey(KeyCode.DownArrow)){
+			rigidbody.velocity = new Vector3(0,-1,0);
+		}
+		else if (Input.GetKey(KeyCode.RightArrow)){
+			rigidbody.velocity = new Vector3(1,0,0);
+		}
+		else if (Input.GetKey(KeyCode.LeftArrow)){
+			rigidbody.velocity = new Vector3(-1,0,0);
+		}
+		else{
+			rigidbody.velocity = new Vector3(0,0,0);
+		}
+	}
+}
