f19ddbd35e56100627b9f0e7b3b71477401a2997
Bjarke Larsen <bjarke@larsen-dk.dk>
2014-03-09 12:58:27 +0100

    created a level 1 and increased hero movement speed

--- DungeonCrawler/Assets/Scripts/HeroMovement.cs
+++ DungeonCrawler/Assets/Scripts/HeroMovement.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class HeroMovement : MonoBehaviour {
 
+
+	float heroSpeed = 5.0f;
+
 	// Use this for initialization
 	void Start () {
 
@@ -13,28 +16,28 @@ public class HeroMovement : MonoBehaviour {
 	void Update () {
 		//Velocity vectors to give movement to the hero
 		if (Input.GetKey(KeyCode.UpArrow)){
-			rigidbody.velocity = new Vector3(0,1,0);
+			rigidbody.velocity = new Vector3(0,heroSpeed,0);
 			if (Input.GetKey (KeyCode.RightArrow)) {
-				rigidbody.velocity = new Vector3(1,1,0);
+				rigidbody.velocity = new Vector3(heroSpeed,heroSpeed,0);
 			}
 			else if (Input.GetKey(KeyCode.LeftArrow)){
-				rigidbody.velocity = new Vector3(-1,1,0);
+				rigidbody.velocity = new Vector3(-heroSpeed,heroSpeed,0);
 			}
 		}
 		else if (Input.GetKey(KeyCode.DownArrow)){
-			rigidbody.velocity = new Vector3(0,-1,0);
+			rigidbody.velocity = new Vector3(0,-heroSpeed,0);
 			if (Input.GetKey(KeyCode.RightArrow)){
-				rigidbody.velocity = new Vector3(1,-1,0);
+				rigidbody.velocity = new Vector3(heroSpeed,-heroSpeed,0);
 			}
 			else if (Input.GetKey(KeyCode.LeftArrow)){
-				rigidbody.velocity = new Vector3(-1,-1,0);
+				rigidbody.velocity = new Vector3(-heroSpeed,-heroSpeed,0);
 			}
 		}
 		else if (Input.GetKey(KeyCode.RightArrow)){
-			rigidbody.velocity = new Vector3(1,0,0);
+			rigidbody.velocity = new Vector3(heroSpeed,0,0);
 		}
 		else if (Input.GetKey(KeyCode.LeftArrow)){
-			rigidbody.velocity = new Vector3(-1,0,0);
+			rigidbody.velocity = new Vector3(-heroSpeed,0,0);
 		}
 		else{
 			rigidbody.velocity = new Vector3(0,0,0);
