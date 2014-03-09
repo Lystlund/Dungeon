b90193b9e3409c1f732d8b07674f84742cd58738
Lystlund <raslystlund@gmail.com>
2014-03-09 13:09:33 +0100

    Door Behavior
    A script for doors to open and close have been written.

---
+++ DungeonCrawler/Assets/Scripts/DoorBehavior.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorBehavior : MonoBehaviour {
+	public Texture DoorOpen;
+	public Texture DoorClosed;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.renderer.material.mainTexture = DoorClosed;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float posX = transform.position.x;
+		float posY = transform.position.y;
+		float posZ = transform.position.z;
+		print(posX);
+		print(posY);
+		print(posZ);
+
+		if (Input.GetKey(KeyCode.E)){
+			gameObject.renderer.material.mainTexture = DoorOpen;
+
+			transform.localPosition = new Vector3(posX,posY,posZ+1);
+		}
+	}
+}
