bc733cd515504dba27bdb907b7f46f7fd049935d
moela87 <morteneland@gmail.com>
2014-03-12 17:25:59 +0100

    EnemyClass

---
+++ DungeonCrawler/Assets/Scripts/EnemyClass.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+	float EnemyStrenght;
+	float EnemyToughness;
+	float EnemyDexterity;
+	float EnemyReflex;
+	float EnemyHealth;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
