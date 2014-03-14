4af6480917e89c375bfcbb27edb5c51c73a8f1cf
Lystlund <raslystlund@gmail.com>
2014-03-14 22:54:41 +0100

    Updated Hero & Hero Scripts
    The Hero's sprite have been finished for now, to make the hero feel more
    alive rotation angles have been added to make the herolook the way the
    hero is walking. Because of this change the camera had to be taken away
    from the hero to keep that from rotating, but to keep the camera on the
    hero a new script CameraFollow was written.

---
+++ DungeonCrawler/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public float LockedZ = -10.0f;
+
+	public Transform target;
+
+	// Update is called once per frame
+	void Update () {
+	
+		transform.position = new Vector3(target.position.x, target.position.y, LockedZ);
+	}
+}
+
