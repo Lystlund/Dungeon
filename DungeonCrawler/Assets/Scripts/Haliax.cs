9b0e9b4302dbedf335062aa70e487b560631d438
moela87 <morteneland@gmail.com>
2014-03-14 15:33:52 +0100

    SpaceZombie and Boss Haliax added

---
+++ DungeonCrawler/Assets/Scripts/Haliax.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Haliax : Enemy {
+
+	public GameObject hero;
+	HeroMovement heroScript;
+	float characterlevel;
+
+	// Use this for initialization
+	void Start () {
+		hero = GameObject.FindWithTag ("Player");
+		heroScript = hero.GetComponent<HeroMovement> ();
+		characterlevel = heroScript.heroLevel;
+		
+		EnemyStrength = 25;
+		EnemyToughness = 25;
+		EnemyDexterity = 25;
+		EnemyReflex = 25;
+		EnemyHealth = 2500;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (EnemyStrength);
+	
+	}
+}
---
+++ DungeonCrawler/Assets/Scripts/SpaceZombie.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceZombie : Enemy {
+
+	public GameObject hero;
+	HeroMovement heroScript;
+	float characterlevel;
+
+	// Use this for initialization
+	void Start () {
+		hero = GameObject.FindWithTag ("Player");
+		heroScript = hero.GetComponent<HeroMovement> ();
+		characterlevel = heroScript.heroLevel;
+		
+		EnemyStrength = 6;
+		EnemyToughness = 6;
+		EnemyDexterity = 1;
+		EnemyReflex = 1;
+		EnemyHealth = 300;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (EnemyStrength);
+	
+	}
+}
