ca82c2f367361076db648dd15ad70f641673f48b
moela87 <morteneland@gmail.com>
2014-03-14 13:32:05 +0100

    Rat script

---
+++ DungeonCrawler/Assets/Scripts/RatScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RatScript : Enemy {
+
+	// Use this for initialization
+	void Start () {
+		EnemyStrength = 2;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (EnemyStrength);
+	
+	}
+}
