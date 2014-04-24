8a99585140f353b23795eec6faf1ff1e591e8eb0
Lystlund <raslystlund@gmail.com>
2014-04-24 13:55:44 +0200

    Cleaned up in the project + added Aggro script
    Added a beginning to an aggro script
    Cleaned up the project

---
+++ DungeonCrawler/Assets/Scripts/Aggro.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Aggro : MonoBehaviour {
+
+	public Transform target;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 dir = target.position - transform.position;
+		float angle = Mathf.Atan2(dir.y,dir.x) * Mathf.Rad2Deg-180;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+	}
+}
