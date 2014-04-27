50d0b70ff3fde03cfe09e0b2a61dc5210b4dd739
Bjarke Larsen <bjarke@larsen-dk.dk>
2014-04-27 16:45:36 +0200

    more fixes

---
+++ DungeonCrawler/Assets/Scripts/endsceneScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class endsceneScript : MonoBehaviour {
+
+	public bool isExitButton = false;
+	
+	void OnMouseEnter(){
+		//Change color of text when moused over
+		guiText.material.color = Color.green;
+	}
+	
+	void OnMouseExit(){
+		//Change color back to original color when the mouse goes away from the text
+		guiText.material.color = Color.white;
+	}
+	
+	void OnMouseUp(){
+		//Check if we are looking at Quit Button, when the mouse goes back up.
+			//load level
+			Application.LoadLevel(0);
+	}
+}
+
--- DungeonCrawler/Assets/Scripts/victoryScript2.cs
+++ DungeonCrawler/Assets/Scripts/victoryScript2.cs
@@ -6,6 +6,6 @@ public class victoryScript2 : MonoBehaviour {
 
 	public void setText(int l){		//Sets the text info to the correct level. Made this in two so this script controls the guiText directly.
 		Debug.Log ("SETTING TEXT IN 2");
-		guiText.text = "Final Level: "+l;
+		guiText.text = "Hero Level: "+l;
 	}
 }
